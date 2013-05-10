7669cf1ed1f9c4cb1c091571c40a955bd25a0855
caiguanhao <caiguanhao@gmail.com>
2013-05-10 16:10:26 +0800

    bash add comments and check file existance;

--- generate_default_configs.sh
+++ generate_default_configs.sh
@@ -100,8 +100,15 @@ do
 		echo "$CONFIGS/$SITE created"
 	fi
 	if [[ ! -f "$CONFIGS/$SITE/_config.yml" ]]; then
+		# clone config file
 		cp "$(pwd)/sample_config.yml" "$CONFIGS/$SITE/_config.yml"
 		
+		if [[ ! -f "$CONFIGS/$SITE/_config.yml" ]]; then
+			echo "$CONFIGS/$SITE/_config.yml is NOT OK."
+			exit
+		fi
+		
+		# replace url in config file
 		sed -i.bak "s/url: http:\/\/www\.qnn\.com\.cn/url: http:\/\/www\.$SITE/g" "$CONFIGS/$SITE/_config.yml"
 		rm -f "$CONFIGS/$SITE/_config.yml.bak"
 		
