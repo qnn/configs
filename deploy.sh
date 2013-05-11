dc99742709cbe5849bd51397498889b531146162
caiguanhao <caiguanhao@gmail.com>
2013-05-11 14:50:45 +0800

    deploy script use parallel processes;

--- deploy.sh
+++ deploy.sh
@@ -15,30 +15,19 @@ IFS=$'\n'
 WEBSITES=($(cat "$(pwd)/WEBSITES"))
 IFS=$OLD_IFS
 
-COUNT=1
-LENGTH=${#WEBSITES[@]}
-
 for WEBSITE in "${WEBSITES[@]}"
 do
 
-	echo "Processing $WEBSITE ($COUNT out of $LENGTH) ..."
-	
 	if [[ ! -d "$SITES/$WEBSITE" ]]; then
 		mkdir "$SITES/$WEBSITE"
 	fi
 	
-	cd "$SITES/$WEBSITE"
-	
 	if [[ ! -d "$SITES/$WEBSITE/.git" ]]; then
-		$GIT clone git://github.com/qnn/template.git "$SITES/$WEBSITE"
+		cd "$SITES/$WEBSITE" && $GIT clone git://github.com/qnn/template.git "$SITES/$WEBSITE" &
 	else
-		$GIT pull origin master
+		cd "$SITES/$WEBSITE" && $GIT pull origin master &
 	fi
-	
-	COUNT=$[COUNT+1]
-	
-	sleep 1
-	
-	echo
 
 done
+
+wait
