27372446a00780865deeb70a3fee6a8e12d8e875
caiguanhao <caiguanhao@gmail.com>
2013-05-11 21:42:57 +0800

    default config generation script add file chooser;

--- generate_default_configs.sh
+++ generate_default_configs.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
-WEBSITES=($(cat "$(pwd)/WEBSITES"))
+SITELIST=(
+	$(find "`pwd`" -type f -maxdepth 1 -name "TODO*")
+	$(find "`pwd`" -type f -maxdepth 1 -name "WEBSITES*")
+)
+
+echo "Found ${#SITELIST[@]} files. Which one to use?"
+
+NO=0;
+for SITE in "${SITELIST[@]}"
+do
+	echo "  $NO) ${SITE##*/}"
+	((NO++))
+done
+
+read NO
+
+if [[ $NO =~ ^[0-9]+$ ]] && [[ $NO -ge 0 ]] && [[ $NO -lt ${#SITELIST[@]} ]]; then
+	FILE=${SITELIST[$NO]}
+else
+	echo "No input file."
+	exit
+fi
+
+echo "Using $FILE."
+
+OLD_IFS=$IFS
+IFS=$'\n'
+WEBSITES=($(cat $FILE))
+IFS=$OLD_IFS
+
+LENGTH=${#WEBSITES[@]}
+
+if [[ LENGTH -eq 0 ]]; then
+	echo "File is empty."
+	exit
+fi
 
 CONFIGS="$(pwd)/configs"
 
@@ -35,3 +70,5 @@ do
 		fi
 	fi
 done
+
+echo "Done."
