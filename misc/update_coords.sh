94c922c20e508489ea67420f17fcd3c3d4173520
caiguanhao <caiguanhao@gmail.com>
2013-05-14 01:03:18 +0800

    add update_coords script; add bdmaps-utils as submodule;

---
+++ misc/update_coords.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -e
+
+CONFIGS=$(cd ../configs && pwd)
+
+ALL_CONFIGS=($(find $CONFIGS -name "_config.yml"))
+
+if [[ ${#ALL_CONFIGS[@]} -lt 1 ]]; then
+	echo "No config files."
+	exit
+else
+	echo "Found ${#ALL_CONFIGS[@]} config files in ${CONFIGS}."
+fi
+
+BASH=$(which bash)
+
+XARGS=$(which xargs)
+
+PRINT()
+{
+	let COL=$(tput cols)-${#1}
+	printf "%s%${COL}s\n" "$1" "[$2]"
+}
+
+for CONFIG in "${ALL_CONFIGS[@]}"
+do
+	ADDR=$(grep '^addr: 地址：' $CONFIG |
+		   sed 's/addr: 地址：\(.*\)/\1/' |
+		   sed 's/\(.*\)[(（].*[)）]/\1/')
+	if [[ ${#ADDR} -gt 5 ]]; then
+		PRINT "${CONFIG}" "PROCESSING"
+		COORD=$($BASH $(find "`pwd`" -name "addr2geo.sh") ${ADDR} |
+			    $XARGS $BASH $(find "`pwd`" -name "geo2point.sh") |
+			    $XARGS $BASH $(find "`pwd`" -name "point2coord.sh"))
+		printf "\e[1A"
+		if [[ $COORD =~ ^[0-9]+\.[0-9]+,\ [0-9]+\.[0-9]+$ ]]; then
+			sed -i.bak "s/coord: .*/coord: ${COORD}/g" "$CONFIG"
+			rm -f "$CONFIG.bak"
+			PRINT "${CONFIG}" "DONE"
+		else
+			PRINT "${CONFIG}" "FAILED"
+			exit
+		fi
+	fi
+done
