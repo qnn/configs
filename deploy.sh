c5b2a10d04f2183b338d7e9ecb345ab52ae9e067
caiguanhao <caiguanhao@gmail.com>
2013-05-15 13:15:40 +0800

    deploy script add update current repo;

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,20 @@
 
 set -e
 
-SOURCE="$(pwd)/source"
-
 GIT=$(which git)
+
+CURRENT="$(pwd)"
+
+echo "Updating $CURRENT ..."
+cd "$CURRENT" && $GIT pull origin master
+
+SOURCE="$(pwd)/source"
 
 if [[ ! -d "$SOURCE" ]]; then
 	mkdir "$SOURCE"
 fi
 
+echo "Updating $SOURCE ..."
 if [[ ! -d "$SOURCE/.git" ]]; then
 	$GIT clone git://github.com/qnn/template.git "$SOURCE"
 else
