5c2751f32fba0d605da144fd9abebdc44f320258
caiguanhao <caiguanhao@gmail.com>
2013-05-29 20:07:18 +0800

    update web hooks;

--- hooks/jekyll-build.php
+++ hooks/jekyll-build.php
@@ -1,6 +1,8 @@
 <?php
 $jekyll = '/usr/local/bin/jekyll';
 
+$git = '/usr/bin/git';
+
 $base = realpath('../');
 
 if (isset($_POST['payload'])) {
@@ -20,14 +22,14 @@ if (isset($_POST['payload'])) {
 	
 	foreach ($all as $A) {
 		if (preg_match('#^configs/(.*)/_config\.yml$#', $A, $matches)) {
-			array_push($commands, $jekyll.' build --source "'.$base.'/source" --destination "'.$base.'/sites/'.$matches[1].'" --config "'.$base.'/source/_config.yml","'.$base.'/configs/'.$matches[1].'/_config.yml"');
+			array_push($commands, $jekyll.' build --source "'.$base.'/source" --destination "'.$base.'/sites/'.$matches[1].'" --config "'.$base.'/source/_config.yml","'.$base.'/configs/'.$matches[1].'/_config.yml" 2>&1');
 		}
 	}
 	
 	if (count($commands) > 0) {
-		$buffer = '['.date('Y-m-d H:i:s O').'] Start ->'."\n";
-		array_unshift($commands, 'git pull origin master');
-		array_unshift($commands, $jekyll.' --version');
+		$buffer = '['.date('Y-m-d H:i:s O').'] Start ->'."\n\n";
+		array_unshift($commands, $git.' pull origin master 2>&1');
+		array_unshift($commands, $jekyll.' --version 2>&1');
 		
 		foreach ($commands as $command) {
 			$buffer .= '['.date('Y-m-d H:i:s O').'] $ '.$command."\n";
@@ -38,5 +40,9 @@ if (isset($_POST['payload'])) {
 		$buffer .= '['.date('Y-m-d H:i:s O').'] <- End'."\n\n";
 		
 		file_put_contents('jekyll-build-'.date('Y-m').'.log', $buffer, FILE_APPEND);
+		
+		shell_exec($git.' add --all '.$base.'/hooks/*.log 2>&1');
+		shell_exec($git.' commit --author "caiguanhao <caiguanhao@gmail.com>" -m "Automatically update jekyll build log files. [WEB HOOKS]" 2>&1');
+		shell_exec($git.' push origin master 2>&1');
 	}
 }
